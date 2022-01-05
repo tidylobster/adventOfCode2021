363580e68b773ba4461cd9c83861f81ba8a090ef
Ilnur Garifullin <ilnur.garifullin@coins.ph>
2022-01-05 19:55:59 +0300

    Day 9

---
+++ src/main/day9/Day9.scala
@@ -0,0 +1,63 @@
+package main.day9
+
+import main.Utils
+
+case class Matrix(data: Array[Array[Int]]) {
+  val width: Int = data.head.length
+  val height: Int = data.length
+  def get(point: Point): Int = data(point.y)(point.x)
+}
+
+case class Point(x: Int, y: Int) {
+  def left: Option[Point] = if (x - 1 >= 0) Some(Point(x - 1, y)) else None
+  def up: Option[Point] = if (y - 1 >= 0) Some(Point(x, y - 1)) else None
+  def right(matrix: Matrix): Option[Point] = if (x + 1 < matrix.width) Some(Point(x + 1, y)) else None
+  def down(matrix: Matrix): Option[Point] = if (y + 1 < matrix.height) Some(Point(x, y + 1)) else None
+
+  def getAdjacentPoints(matrix: Matrix): Seq[Point] =
+    Seq(left, up, right(matrix), down(matrix)).filter(_.isDefined).map(_.get)
+}
+
+object Day9 extends App with Utils {
+  val data: String = getData("src/resources/Day9.txt")
+  val matrix: Matrix = Matrix(data.split("\n").map(_.split("").map(_.toInt)))
+
+  private def getLowestPoints(matrix: Matrix): Seq[Point] =
+    for {
+      i <- matrix.data.indices
+      j <- matrix.data(i).indices if isLowest(Point(j, i), matrix)
+    } yield Point(j, i)
+
+  private def isLowest(value: Int, adjacent: Seq[Int]): Boolean =
+    value == (value +: adjacent).min && value != adjacent.min
+
+  private def isLowest(point: Point, matrix: Matrix): Boolean = {
+    val adjacent = point.getAdjacentPoints(matrix)
+    isLowest(matrix.get(point), adjacent.map(matrix.get))
+  }
+
+  def part1(matrix: Matrix): Int =
+    getLowestPoints(matrix).map(matrix.get).map(_ + 1).sum
+
+  private def getBasinPoints(matrix: Matrix)(point: Point): Seq[Point] = {
+    def loop(point: Point, basin: Set[Point]): Set[Point] =
+      if (basin.contains(point))
+        basin
+      else
+        point.getAdjacentPoints(matrix).foldLeft(basin ++ Set(point)) {
+          case (basin, point) if matrix.get(point) == 9 => basin
+          case (basin, point) if basin.contains(point) => basin
+          case (basin, point) => loop(point, basin)
+        }
+      loop(point, Set.empty).toSeq
+    }
+
+  def part2(matrix: Matrix): Int = {
+    val lowestPoints = getLowestPoints(matrix)
+    val basins = lowestPoints.map(getBasinPoints(matrix)).map(_.size)
+    basins.sorted(Ordering[Int].reverse).take(3).product
+  }
+
+  println(part1(matrix))
+  println(part2(matrix))
+}
